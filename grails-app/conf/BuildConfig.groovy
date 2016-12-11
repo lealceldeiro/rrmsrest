e96807b4df27dc7e7304e76616f4af206507051e
Asiel <lealceldeiro@gmail.com>
2016-12-11 14:55:36 -0500

    Added cors support (cors grails plugin)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -66,6 +66,7 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
+        runtime "org.grails.plugins:cors:1.1.8"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
